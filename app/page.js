6c73df491a06addc435a8a2b1362b883ed3c9779
ydideh810 <yd.baller810@gmail.com>
2024-07-31 13:24:17 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -51,9 +51,9 @@ export default function Home() {
   return (
     <div className="container max-w-2xl mx-auto p-5">
       <h1 className="py-6 text-center font-bold text-2xl">
-        Dream something with{" "}
-        <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
-          SDXL
+        Explore the Cosmos with{" "}
+        <a href="https://replicate.com/ydideh810/cosmo-speak">
+          CosmoSpeak
         </a>
       </h1>
 
@@ -62,7 +62,7 @@ export default function Home() {
           type="text"
           className="flex-grow"
           name="prompt"
-          placeholder="Enter a prompt to display an image"
+          placeholder="Anything about Space/Aeronautics engineering"
           ref={promptInputRef}
         />
         <button className="button" type="submit">
